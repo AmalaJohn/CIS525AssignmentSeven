8c951446fe29754e67acd9418d8f64168a3673dc
amalaj <amalaj@cougar.cs.ksu.edu>
2025-11-19 14:27:44 -0600

    added TLS

--- directoryServer5.c
+++ directoryServer5.c
@@ -1,3 +1,10 @@
+/* directoryServer5.c
+ * Minimal changes from Assignment4: add TLS (OpenSSL TLS 1.3), non-blocking sockets,
+ * SSL_read/SSL_write, and store client IP at accept time.
+ *
+ * Preserves original flow and variable names wherever possible.
+ */
+
 #include <stdio.h>
 #include <string.h>
 #include <strings.h>
@@ -5,6 +12,14 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/queue.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include <errno.h>
+
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+
 #include "inet.h"
 #include "common.h"
 
@@ -18,6 +33,7 @@
 
 struct client {
 	int socketfd;
+	SSL *ssl;                    /* TLS handle for this connection */
 	char username[MAX];
 	int client_port;
 	struct in_addr client_ip;
@@ -26,6 +42,13 @@ struct client {
 };
 LIST_HEAD(client_list, client) clients;
 
+
+/* tiny helper to set non-blocking (minimize added helpers as requested) */
+static int make_nonblocking(int fd) {
+    int flags = fcntl(fd, F_GETFL, 0);
+    if (flags < 0) return -1;
+    return fcntl(fd, F_SETFL, flags | O_NONBLOCK);
+}
 
 int username_exists(const char *name) {
     struct client *c;
@@ -43,14 +66,47 @@ int main(int argc, char **argv)
 	struct sockaddr_in cli_addr, serv_addr;
 	fd_set readset;
 
+	/* Initialize OpenSSL */
+	SSL_library_init();
+	SSL_load_error_strings();
+	OpenSSL_add_all_algorithms();
+
+	/* Create SSL context (server) */
+	SSL_CTX *ctx = SSL_CTX_new(TLS_server_method());
+	if (!ctx) {
+		fprintf(stderr, "Unable to create SSL context\n");
+		ERR_print_errors_fp(stderr);
+		return EXIT_FAILURE;
+	}
+	/* Enforce TLS 1.3 only */
+	SSL_CTX_set_min_proto_version(ctx, TLS1_3_VERSION);
+	SSL_CTX_set_max_proto_version(ctx, TLS1_3_VERSION);
+
+	/* Do not require client certs (assignment said client auth not required) */
+	SSL_CTX_set_verify(ctx, SSL_VERIFY_NONE, NULL);
+
+	/* Load server cert and key */
+	if (SSL_CTX_use_certificate_file(ctx, "certs/directory_server.crt", SSL_FILETYPE_PEM) <= 0) {
+		ERR_print_errors_fp(stderr);
+		return EXIT_FAILURE;
+	}
+	if (SSL_CTX_use_PrivateKey_file(ctx, "certs/directory_server.key", SSL_FILETYPE_PEM) <= 0) {
+		ERR_print_errors_fp(stderr);
+		return EXIT_FAILURE;
+	}
+	/* Load CA (for thoroughness; not used for client auth here) */
+	if (!SSL_CTX_load_verify_locations(ctx, "certs/ca.crt", NULL)) {
+		ERR_print_errors_fp(stderr);
+		/* not fatal for this assignment, but warn */
+	}
+
 	/* Create communication endpoint */
 	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
 		perror("server: can't open stream socket");
 		return EXIT_FAILURE;
 	}
 
-	/* Add SO_REUSEADDRR option to prevent address in use errors (modified from: "Hands-On Network
-	* Programming with C" Van Winkle, 2019. https://learning.oreilly.com/library/view/hands-on-network-programming/9781789349863/5130fe1b-5c8c-42c0-8656-4990bb7baf2e.xhtml */
+	/* Add SO_REUSEADDR option */
 	int true = 1;
 	if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, (void *)&true, sizeof(true)) < 0) {
 		perror("server: can't set stream socket address reuse option");
@@ -70,9 +126,13 @@ int main(int argc, char **argv)
 
 	listen(sockfd, 5);
 
+	/* make listening socket non-blocking so accept won't block forever */
+	if (make_nonblocking(sockfd) < 0) {
+		perror("make_nonblocking listen socket");
+		/* not fatal, continue */
+	}
 
 	LIST_INIT(&clients);
-
 
 	for (;;) {
 		/* Initialize and populate your readset and compute maxfd */
@@ -81,17 +141,13 @@ int main(int argc, char **argv)
 		/* We won't write to a listening socket so no need to add it to the writeset */
 		int max_fd = sockfd;
 
-		/* FIXME: Populate readset with ALL your client sockets here,
-		 * e.g., using LIST_FOREACH */
+		/* Populate readset with ALL your client sockets here */
 		struct client *entry;
         LIST_FOREACH(entry, &clients, entries){
 			FD_SET(entry->socketfd, &readset);
 			if (entry->socketfd > max_fd)
 				max_fd = entry->socketfd;
 		}
-
-		/* Compute max_fd as you go */
-		//if (max_fd < clisockfd) {max_fd = clisockfd;}
 
 		if (select(max_fd+1, &readset, NULL, NULL, NULL) > 0) {
 
@@ -103,73 +159,92 @@ int main(int argc, char **argv)
 
 				if (newsockfd < 0) {
 					perror("server: accept error");
-					close (newsockfd);
-					close(sockfd);
-					return EXIT_FAILURE;
-				}
-				/* FIXME: Add newsockfd to your list of clients -- but no nickname yet */
-				struct client *new_client = malloc(sizeof(struct client));
-				if (new_client != NULL){
-					new_client->socketfd = newsockfd;
-					new_client->is_server = 0;
-					new_client->username[0] = '\0';
-					LIST_INSERT_HEAD(&clients, new_client, entries);
-				}
-				else{
-					close(newsockfd);
-					fprintf(stderr, "server: malloc error");
-					close(sockfd);
-					return EXIT_FAILURE;
+					/* on accept error, continue server loop (don't exit entirely) */
+				} else {
+					/* allocate client and set up TLS */
+					struct client *new_client = malloc(sizeof(struct client));
+					if (new_client != NULL){
+						new_client->socketfd = newsockfd;
+						new_client->is_server = 0;
+						new_client->username[0] = '\0';
+						new_client->client_ip = cli_addr.sin_addr; /* store peer ip now */
+						new_client->ssl = SSL_new(ctx);
+						if (!new_client->ssl) {
+							ERR_print_errors_fp(stderr);
+							close(new_client->socketfd);
+							free(new_client);
+						} else {
+							SSL_set_fd(new_client->ssl, newsockfd);
+							/* make non-blocking so TLS operations can return WANT_READ/WANT_WRITE */
+							if (make_nonblocking(new_client->socketfd) < 0) {
+								/* not fatal, continue */
+							}
+							LIST_INSERT_HEAD(&clients, new_client, entries);
+						}
+					}
+					else{
+						close(newsockfd);
+						fprintf(stderr, "server: malloc error");
+					}
 				}
 				/* We can't immediately read(newsockfd) because we haven't asked
 				* select whether it's ready for reading yet */
 			}
-			/* TODO: Check ALL your client sockets, e.g., using LIST_FOREACH */
-			/* clisockfd is used as an example socket -- we never populated it so
-			* it's invalid */
+			/* Check ALL your client sockets */
 			struct client *entry1;
 			struct client *tmp;
 			LIST_FOREACH_SAFE(entry1, &clients, entries,tmp) {
-			/* Note that this is a seperate if, not an else if -- multiple sockets
-			* may become ready */
+				/* Note: multiple sockets may become ready */
 				if (FD_ISSET(entry1->socketfd, &readset)) {
-					//fprintf(stderr, "socket %d is ready\n", entry1->socketfd); //DEBUG
-					/* FIXME: Modify the logic */
-					char buf[MAX];
-
-					/* Read the request from the client */
-					/* FIXME: This may block forever since we haven't asked select
-						whether clisockfd is ready */
-					ssize_t nread = read(entry1->socketfd, buf, MAX);
-					if (nread == 0) {
-						//fprintf(stderr, "Client %d disconnected\n", entry1->socketfd); 
-						close(entry1->socketfd);
-
-						LIST_REMOVE(entry1, entries);
-						free(entry1);
+					/* Perform TLS handshake if not yet finished */
+					if (!SSL_is_init_finished(entry1->ssl)) {
+						int ret = SSL_accept(entry1->ssl);
+						if (ret <= 0) {
+							int err = SSL_get_error(entry1->ssl, ret);
+							if (err == SSL_ERROR_WANT_READ || err == SSL_ERROR_WANT_WRITE) {
+								/* handshake in progress; continue to next client */
+								continue;
+							} else {
+								ERR_print_errors_fp(stderr);
+								/* remove client */
+								SSL_free(entry1->ssl);
+								close(entry1->socketfd);
+								LIST_REMOVE(entry1, entries);
+								free(entry1);
+								continue;
+							}
+						}
+						/* Handshake finished; continue so we can read request on next select */
+						continue;
 					}
-					else if (nread < 0) {
-						/* Not every error is fatal. Check the return value and act accordingly. */
-						if( errno != EAGAIN && errno != EWOULDBLOCK && errno != EINTR) {
-							fprintf(stderr, "%s:%d Error reading from client\n", __FILE__, __LINE__);
-							close (entry1->socketfd);
+
+					/* Read the request from the client over TLS */
+					char buf[MAX+1];
+					int nread = SSL_read(entry1->ssl, buf, MAX);
+					if (nread <= 0) {
+						int err = SSL_get_error(entry1->ssl, nread);
+						if (nread == 0 || (err != SSL_ERROR_WANT_READ && err != SSL_ERROR_WANT_WRITE)) {
+							/* connection closed or fatal error */
+							SSL_shutdown(entry1->ssl);
+							SSL_free(entry1->ssl);
+							close(entry1->socketfd);
 							LIST_REMOVE(entry1, entries);
 							free(entry1);
-							return EXIT_FAILURE;
 						}
+						/* else WANT_READ/WRITE -> try later */
 					}
-					else{
-						//fprintf(stderr, "Received from client: %s\n", buf); //DEBUG
+					else {
 						buf[nread] = '\0';
 						char command[32];
 						char payload[92];
 						int port = 0;
 						if (sscanf(buf, "%31[^:]:%63[^:]:%d", command, payload, &port) == 3) {
-							//fprintf(stderr, "Parsed command='%s', payload='%s'\n", command, payload);
-
 							if (strncmp(command, "USERNAME",8) == 0) {
 								if (username_exists(payload)) {
-									write(entry1->socketfd, "ERROR: Username already taken\n", 31);
+									const char *msg = "ERROR: Username already taken\n";
+									SSL_write(entry1->ssl, msg, strlen(msg));
+									SSL_shutdown(entry1->ssl);
+									SSL_free(entry1->ssl);
 									close(entry1->socketfd);
 									LIST_REMOVE(entry1, entries);
 									free(entry1);
@@ -178,13 +253,11 @@ int main(int argc, char **argv)
 									snprintf(entry1->username, MAX, "%s", payload);
 									entry1->is_server = 1;
 									entry1->client_port = htons(port);
-									entry1->client_ip = cli_addr.sin_addr;
-									//fprintf(stderr,"Registered new server!"); //DEBUG
-		
+									/* preserve your original approach but use stored client_ip */
 								}
 							}
-							else if (strncmp(command, "CHAT", 3) == 0) {
-								char response[MAX];								
+							else if (strncmp(command, "CHAT", 4) == 0) {
+								char response[MAX];
 								int offset = 0;  // Keeps track of how many bytes we've written
 								int remaining = MAX;
 								int count = 0;
@@ -217,12 +290,13 @@ int main(int argc, char **argv)
 											"No registered chat rooms right now\n");
 								}
 
-								write(entry1->socketfd, response, sizeof(response));
+								/* write only the used portion */
+								SSL_write(entry1->ssl, response, strlen(response));
 							}	
 							else {
-								write(entry1->socketfd, "Incorrect command\n", 19);
+								const char *msg = "Incorrect command\n";
+								SSL_write(entry1->ssl, msg, strlen(msg));
 							}
-
 						}
 					}
 				}
@@ -230,7 +304,7 @@ int main(int argc, char **argv)
 		}
 		else {
 			if (errno == EINTR) {
-				// Interrupted by a signal (like SIGINT), just restart loop
+				/* Interrupted by a signal (like SIGINT), just restart loop */
 				continue;
 			} else {
 				perror("select error");
@@ -239,4 +313,6 @@ int main(int argc, char **argv)
 		}
 	}
 	close(sockfd);
+	SSL_CTX_free(ctx);
+	return 0;
 }
